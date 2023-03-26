de7ec6c49f16966f5dc9d45334d3cf4e8c0a1f87
piglaker <zxt235813@163.com>
2023-03-26 14:16:31 +0000

    install & run

--- docker_run_retrieval.sh
+++ docker_run_retrieval.sh
@@ -1,6 +1,7 @@
 version="7"
 ##--net=host\
 #--env http_proxy="http://10.176.52.116:7890" --env https_proxy="http://10.176.52.116:7890"
+#--env https_proxy="http://10.176.50.14:7890" --env http_proxy="http://10.176.50.14:7890" --env all_proxy="socks5://10.176.50.14:7891"
 docker run -P --rm --name="retrieval" --shm-size=200g \
 -w /retrieval -p7004:7004  \
 -v `pwd`:/retrieval piglake/retrieval:0.${version} \
