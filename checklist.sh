a8be0c3b7a732dac9350e1a1e1b0be6e5233bc73
piglaker <zxt235813@163.com>
2023-04-22 08:49:25 +0000

    clean

--- checklist.sh
+++
@@ -1,9 +0,0 @@
-http http://127.0.0.1:7004/
-
-netstat -s | egrep "listen|LISTEN"
-
-iptables -L -v
-
-tcpdump port 8000 -i lo
-
-ss -lnt
--- run.sh
+++
@@ -1,1 +0,0 @@
-nohup python3 retrieval_backend.py --port 7004 --timeout 20000 --debug 2>&1 > h.log & 
