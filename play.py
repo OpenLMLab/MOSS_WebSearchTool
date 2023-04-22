842b0afcad5a33bd1e4cbd89e110838a077891c2
piglaker <zxt235813@163.com>
2023-04-22 08:40:06 +0000

    clean

--- play.py
+++
@@ -1,9 +0,0 @@
-import time
-
-start_time = time.time()
-
-while True:
-    if (time.time() - start_time) % 5 == 0:
-        print(1)
-
-
--- test_get.sh
+++
@@ -1,3 +0,0 @@
-date
-wget https://www.usnews.com/education/best-global-universities/fudan-university-500824
-date
